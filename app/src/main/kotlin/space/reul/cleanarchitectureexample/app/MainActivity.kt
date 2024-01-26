8820731347b21c914a808e890bfa9b350f720e53
Tiago A. Reul <tiago@reul.space>
2024-01-26 09:56:50 -0300

    Extract composables to their own files.

--- app/src/main/kotlin/space/reul/cleanarchitectureexample/app/MainActivity.kt
+++ app/src/main/kotlin/space/reul/cleanarchitectureexample/app/MainActivity.kt
@@ -4,24 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import space.reul.cleanarchitectureexample.app.ui.composables.ImageGrid
 import space.reul.cleanarchitectureexample.app.ui.theme.CleanArchitectureExampleTheme
-import space.reul.cleanarchitectureexample.app.ui.theme.Spacing
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainActivityViewModel by viewModels()
@@ -51,34 +42,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun ImageGrid(urls: List<String>, modifier: Modifier = Modifier) {
-    LazyVerticalGrid(
-        modifier = modifier,
-        columns = GridCells.Adaptive(150.dp)
-    ) {
-        urls.forEach { url ->
-            item {
-                Card(
-                    Modifier
-                        .padding(Spacing.quarter)
-                        .height(150.dp)) {
-                    AsyncImage(
-                        modifier = Modifier.fillMaxHeight(),
-                        model = url,
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ShibaGridPreview() {
-    CleanArchitectureExampleTheme {
-        ImageGrid(listOf("https://cdn.shibe.online/shibes/36083b6b1f07865085681235e4b4b174f60b7db1.jpg"))
-    }
-}
